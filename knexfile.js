c6b48486096bf6b53dbcbfc970278b618b006cb0
Peterson Normil <78240344+pistolPeterson@users.noreply.github.com>
2025-01-08 20:57:32 -0500

    fix db deployment

--- knexfile.js
+++ knexfile.js
@@ -1,53 +1,34 @@
-const path = require("path");
-
 require("dotenv").config();
-
 const {
-  DATABASE_URL = "postgresql://postgres@localhost/postgres",
+  NODE_ENV = "development",
+  DEVELOPMENT_DATABASE_URL,
+  PRODUCTION_DATABASE_URL,
 } = process.env;
+const URL =
+  NODE_ENV === "production"
+    ? PRODUCTION_DATABASE_URL
+    : DEVELOPMENT_DATABASE_URL;
 
 module.exports = {
   development: {
     client: "postgresql",
-    connection: DATABASE_URL,
-    pool: { min: 0, max: 5 },
+    connection: URL,
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: __dirname + "/src/db/migrations",
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: __dirname + "/src/db/seeds",
     },
-    ssl: {
-      rejectUnauthorized: false // This will allow connections without requiring SSL certificates to be valid.
-    }
   },
 
   production: {
     client: "postgresql",
-    connection: DATABASE_URL,
-    pool: { min: 0, max: 5 },
+    connection: URL,
     migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
+      directory: __dirname + "/src/db/migrations",
     },
     seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
+      directory: __dirname + "/src/db/seeds",
     },
-    ssl: {
-      rejectUnauthorized: false // This will allow connections without requiring SSL certificates to be valid.
-    }
-  },
-
-  test: {
-    client: "sqlite3",
-    connection: {
-      filename: ":memory:",
-    },
-    migrations: {
-      directory: path.join(__dirname, "src", "db", "migrations"),
-    },
-    seeds: {
-      directory: path.join(__dirname, "src", "db", "seeds"),
-    },
-    useNullAsDefault: true,
   },
 };
